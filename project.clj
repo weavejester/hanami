64476c330aa27a9dac20e5b2e95180384468e9fa
James Reeves <jreeves@weavejester.com>
2014-12-20 21:54:44 +0000

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hanami "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A utility library for Heroku web applications"
+  :url "https://github.com/weavejester/hanami"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
